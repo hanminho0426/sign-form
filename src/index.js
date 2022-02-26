2bdfa0c724df7fc6c98a3922ffb7c514830f9fcc
Minofe <l9930426@naver.com>
2022-02-26 23:00:13 +0900

    'start'

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// const id = document.getElementById('id')
+// window.addEventListener('load', () => {
+//     id.focus()
+// })
+// JS로 오토포커스 기능을 만들어도 되지만
+//Html의 input의 autofocus기능으로 간단하게 할수도 있다.

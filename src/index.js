1a10f68578501d1f412b1dfbb2d395f3d4e34c1f
Minofe <l9930426@naver.com>
2022-03-03 13:28:16 +0900

    '추가'

--- src/index.js
+++ src/index.js
@@ -27,14 +27,16 @@ const PW_CHECK_ERROR_MSG = {
     required: '필수 정보입니다.',
     invalid: '비밀번호가 일치하지 않습니다.',
 }
+const checkIdRegex = (value) => {
+    if (value.length === 0) {
+        return 'required'
+    } else {
+        return ID_REGEX.test(value) ? true : 'invalid'
+    }
+}
 
 const confirmID = (value) => {
-    let checkID
-    if (value.length === 0) {
-        checkID = 'required'
-    } else {
-        checkID = ID_REGEX.test(value) ? true : 'invalid'
-    }
+    const checkID = checkIdRegex(value)
     if (checkID !== true) {
         id.classList.add('border-red-600')
         idMsg.innerText = ID_ERROR_MSG[checkID]
@@ -42,16 +44,21 @@ const confirmID = (value) => {
         id.classList.remove('border-red-600')
         id.innerText = ''
     }
+    return checkID
 }
+
 id.addEventListener('focusout', (event) => confirmID(event.target.value))
 
-const confirmPW = (value) => {
-    let checkPW
+const checkPwRegex = (value) => {
     if (value.length === 0) {
-        checkPW = 'required'
+        return 'required'
     } else {
-        checkPW = PW_REGEX.test(value) ? true : 'invalid'
+        return PW_REGEX.test(value) ? true : 'invalid'
     }
+}
+
+const confirmPW = (value) => {
+    const checkPW = checkPwRegex(value)
     if (checkPW !== true) {
         pw.classList.add('border-red-600')
         pwMsg.innerText = PW_ERROR_MSG[checkPW]
@@ -59,16 +66,19 @@ const confirmPW = (value) => {
         pw.classList.remove('border-red-600')
         pw.innerText = ''
     }
+    return checkPW
 }
 pw.addEventListener('focusout', (event) => confirmPW(event.target.value))
 
-const confirmCheckPW = (value) => {
-    let checkPW2
+const checkPwCheckRegex = (value) => {
     if (value.length === 0) {
-        checkPW2 = 'required'
+        return 'required'
     } else {
-        checkPW2 = pw.value === value ? true : 'invalid'
+        return value === pw.value ? true : 'invalid'
     }
+}
+const confirmCheckPW = (value) => {
+    let checkPW2 = checkPwCheckRegex(value)
     if (checkPW2 !== true) {
         pwCheck.classList.add('border-red-600')
         pwCheckMsg.innerText = PW_CHECK_ERROR_MSG[checkPW2]
@@ -76,16 +86,21 @@ const confirmCheckPW = (value) => {
         pwCheck.classList.remove('border-red-600')
         pwCheck.innerText = ''
     }
+    return checkPW2
 }
+
 pwCheck.addEventListener('focusout', (event) =>
     confirmCheckPW(event.target.value)
 )
 
+//모달 창 구현
 const submit = document.getElementById('submit')
+const modal = document.getElementById('modal')
 
 submit.addEventListener('click', (event) => {
     event.preventDefault()
-    confirmID(id.value)
-    confirmPW(pw.value)
-    confirmCheckPW(pwCheck.value)
+    const isConfirmForm =
+        confirmID(id.value) &&
+        confirmPW(pw.value) &&
+        confirmCheckPW(pwCheck.value)
 })
